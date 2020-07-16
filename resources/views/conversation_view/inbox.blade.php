5f2b6c7e04056341a962fd29b0b33688265a6672
ebd <e.dehghani@chmail.ir>
2020-07-16 20:21:12 +0430

    final

---
+++ resources/views/conversation_view/inbox.blade.php
@@ -0,0 +1,360 @@
+@extends('layouts.app')
+
+@section('title',__('Provider'))
+
+@push('css')
+    <link href="{{asset('assets/css/dataTables.bootstrap.min.css')}}" rel="stylesheet"/>
+@endpush
+@section('content')
+    @role('Admin|product')
+    <div class="content persian">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header card-header-primary">
+                            <h4 class="card-title text-right font-weight-400">{{__('Provider List')}}</h4>
+                            <p class="card-category"></p>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive table-hover">
+                                <table id="table" class="table" cellspacing="0" width="100%">
+                                    <thead class=" text-primary">
+                                    <th>
+                                        {{__('ID')}}
+                                    </th>
+                                    <th>
+                                        {{__('Name')}}
+                                    </th>
+                                    <th>
+                                        {{__('Phone')}}
+                                    </th>
+                                    <th>
+                                        {{__('Address')}}
+                                    </th>
+                                    <th>
+                                        {{__('Action')}}
+                                    </th>
+                                    </thead>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card card-user">
+                        <div class="card">
+                            <div class="card-header card-header-primary">
+                                <h4 class="card-title ">{{__('New Provider')}}</h4>
+                                <p class="card-category"></p>
+                            </div>
+                            <div class="card-body">
+                                <form id="form1">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>{{__('Name')}}</label>
+                                                <input class="form-control" name="hp_name">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>{{__('Phone')}}</label>
+                                                <input name="hp_phone" type="text" class="form-control" required=""
+                                                       aria-invalid="false">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="form-group">
+                                                <label>{{__('Address')}}</label>
+                                                <input name="hp_address" type="text" class="form-control" required=""
+                                                       aria-invalid="false">
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card-footer">
+                                        <button type="submit" class="btn btn-fill btn-primary">{{__('Save')}}</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card card-user" id="card-form2">
+                        <div class="card">
+                            <div class="card-header card-header-primary">
+                                <h4 class="card-title ">{{__('Edit Provider')}}</h4>
+                                <p class="card-category"></p>
+                            </div>
+                            <div class="card-body">
+                                <form id="form2">
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label>{{__('Name')}}</label>
+                                                    <input id="hp_name" class="form-control"
+                                                           name="hp_name">
+                                                    <input id="provider-id" hidden>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class=" row">
+                                            <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>{{__('Phone')}}</label>
+                                                        <input name="hp_phone" type="text" class="form-control"
+                                                               required=""
+                                                               aria-invalid="false" id="hp_phone">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class=" row">
+                                                <div class="col-md-12">
+                                                    <div class="form-group">
+                                                        <label>{{__('Address')}}</label>
+                                                        <input name="hp_address" type="text" class="form-control"
+                                                               required=""
+                                                               aria-invalid="false" id="hp_address">
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            {{--<div class="row">--}}
+                                            {{--<div class="col-md-6 pr-md-1">--}}
+                                            {{--<div class="form-group">--}}
+                                            {{--<label>{{__('Account Number')}}</label>--}}
+                                            {{--<input name="hp_account_number" disabled--}}
+                                            {{--class="form-control"    value="{{$provider->hp_account_number}}">--}}
+                                            {{--</div>--}}
+                                            {{--</div>--}}
+                                            {{--</div>--}}
+                                            <div class="card-footer">
+                                                <button type="submit"
+                                                        class="btn btn-fill btn-primary">{{__('Save')}}</button>
+                                            </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endrole
+@endsection
+
+@push('scripts')
+    <script src="{{asset('assets/js/jquery.dataTables.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/dataTables.bootstrap.min.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/plugins/jquery.blockUI.js')}}" type="text/javascript"></script>
+    <script src="{{asset('assets/js/sweetalert.min.js')}}"></script>
+    <script>
+        $(document).ready(function () {
+
+            $('#card-form2').hide();
+
+            var elems = Array.prototype.slice.call(document.querySelectorAll('.js-switch'));
+
+            $('#table').on('click', 'button', function (event) {
+
+                var data = table.row($(this).parents('tr')).data();
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                swal({
+                    // title: "",
+                    text: "{{__('Are you sure?')}}",
+                    buttons: ["{{__('cancel')}}", "{{__('Done')}}"],
+                    icon: "warning",
+                    // buttons: true,
+                    dangerMode: true,
+                })
+                    .then((willDelete) => {
+                        if (willDelete) {
+                            $.ajax({
+                                url: '/provider-destroy/' + data[5],
+                                type: 'delete',
+                                data: data,
+                                dataType: 'json',
+                                async: false,
+                                success: function (data) {
+                                    swal("{{__("Poof! Your imaginary file has been deleted!")}}", {
+                                        icon: "success",
+                                        button: "{{__('Done')}}",
+                                    });
+                                },
+                                cache: false,
+                            });
+                            $('#table').DataTable().ajax.reload();
+                        } else {
+                            swal(
+                                "{{__("Your imaginary file is safe!")}}",
+                                {button: "{{__('Done')}}"}
+                            );
+
+                        }
+                    });
+            });
+            var table = $('#table').on('draw.dt', function (e, settings, json, xhr) {
+
+            }).DataTable({
+                "processing":
+                    true,
+                "serverSide":
+                    true,
+                "ajax":
+                    '/json-data-provider',
+                "columnDefs":
+                    [{
+                        "targets": -1,
+                        "data": null,
+
+                        "render": function (data, type, row, meta) {
+                            return "  <div class=\"dropdown\">\n" +
+                                "                                                            <a class=\"btn btn-link dropdown-toggle btn-icon\"\n" +
+                                "                                                                    data-toggle=\"dropdown\">\n" +
+                                "                                                                <i class=\"tim-icons icon-settings-gear-63\"></i>\n" +
+                                "                                                            </a>\n" +
+                                "                                                            <div class=\"dropdown-menu dropdown-menu-right\"\n" +
+                                "                                                                 aria-labelledby=\"dropdownMenuLink\">\n" +
+                                "                                                                <a class=\"dropdown-item edit\"\n" +
+                                "                                                                >{{__('Edit')}}</a>\n" +
+                                "                                                                <button class=\"dropdown-item deleted\" id=\"deleted\" type=\"submit\">{{__('Delete')}}</button>\n" +
+                                "                                                            </div>\n" +
+                                "                                                        </div>"
+                        }
+                    }],
+                "language":
+                    {
+                        "sEmptyTable":
+                            "هیچ داده ای در جدول وجود ندارد",
+                        "sInfo":
+                            "نمایش _START_ تا _END_ از _TOTAL_ رکورد",
+                        "sInfoEmpty":
+                            "نمایش 0 تا 0 از 0 رکورد",
+                        "sInfoFiltered":
+                            "(فیلتر شده از _MAX_ رکورد)",
+                        "sInfoPostFix":
+                            "",
+                        "sInfoThousands":
+                            ",",
+                        "sLengthMenu":
+                            "نمایش _MENU_ رکورد",
+                        "sLoadingRecords":
+                            "در حال بارگزاری...",
+                        "sProcessing":
+                            "در حال پردازش...",
+                        "sSearch":
+                            "جستجو:",
+                        "sZeroRecords":
+                            "رکوردی با این مشخصات پیدا نشد",
+                        "oPaginate":
+                            {
+                                "sFirst":
+                                    "ابتدا",
+                                "sLast":
+                                    "انتها",
+                                "sNext":
+                                    "بعدی",
+                                "sPrevious":
+                                    "قبلی"
+                            }
+                        ,
+                        "oAria":
+                            {
+                                "sSortAscending":
+                                    ": فعال سازی نمایش به صورت صعودی",
+                                "sSortDescending":
+                                    ": فعال سازی نمایش به صورت نزولی"
+                            }
+                    }
+            });
+            // store color
+            $("#form1").submit(function (event) {
+                var data = $("#form1").serialize();
+                event.preventDefault();
+                $("#form1").block({
+                    message: '{{__('please wait...')}}', css: {
+                        border: 'none',
+                        padding: '15px',
+                        backgroundColor: '#000',
+                        '-webkit-border-radius': '10px',
+                        '-moz-border-radius': '10px',
+                        opacity: .5,
+                        color: '#fff'
+                    }
+                });
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+
+                $.ajax({
+                    url: '/provider',
+                    type: 'POST',
+                    data: data,
+                    dataType: 'json',
+                    async: false,
+                    success: function (data) {
+                        setTimeout($("#form1").unblock(), 2000);
+                        document.getElementById('form1').reset();
+                        $('#table').DataTable().ajax.reload();
+                    },
+                    cache: false,
+                });
+            });
+            // update color
+            $("#form2").submit(function (event) {
+                var data = $("#form2").serialize();
+                var provider = $('#provider-id').val();
+                event.preventDefault();
+                $('#form2').block({
+                    message: '{{__('please wait...')}}', css: {
+                        border: 'none',
+                        padding: '15px',
+                        backgroundColor: '#000',
+                        '-webkit-border-radius': '10px',
+                        '-moz-border-radius': '10px',
+                        opacity: .5,
+                        color: '#fff'
+                    }
+                });
+                //token
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: '/provider/' + provider,
+                    type: 'POST',
+                    data: data,
+                    dataType: 'json',
+                    method: 'put',
+                    async: false,
+                    success: function (data) {
+                        setTimeout($('#form2').unblock(), 2000);
+                        $('#table').DataTable().ajax.reload();
+                        $('#card-form2').hide();
+                    },
+                    cache: false,
+                });
+            });
+            // fill data in edit form
+            $('#table').on('click', '.edit', function (event) {
+                $('#card-form2').show();
+                var data = table.row($(this).parents('tr')).data();
+                $('#provider-id').val(data[5]);
+                $('#hp_name').val(data[1]);
+                $('#hp_phone').val(data[2]);
+                $('#hp_address').val(data[3]);
+            })
+            // endfilling
+        });
+    </script>
+@endpush
